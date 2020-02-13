0d447fff9f4252d6197067933ec55021372235fb
azagatti <andre.zagatti1@gmail.com>
2020-02-12 23:59:40 -0300

    Created Index route to show providers

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -1,7 +1,16 @@
 import User from '../models/User';
+import File from '../models/File';
 
 class ProviderController {
-  async index(req, res) {}
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [File],
+    });
+
+    return res.json({ providers });
+  }
 }
 
 export default new ProviderController();
