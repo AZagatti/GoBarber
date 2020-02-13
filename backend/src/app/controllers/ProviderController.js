11e3697fa2167a1e56616e74400ef5287ca25f48
azagatti <andre.zagatti1@gmail.com>
2020-02-13 00:04:40 -0300

    Finished list provider route

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -6,7 +6,13 @@ class ProviderController {
     const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
-      include: [File],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
     });
 
     return res.json({ providers });
