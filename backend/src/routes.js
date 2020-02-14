9bdc23efa9aa96a020a1162a1d21b1cae264c412
azagatti <andre.zagatti1@gmail.com>
2020-02-14 20:56:47 -0300

    Created appointment cancelation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -24,8 +24,9 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
