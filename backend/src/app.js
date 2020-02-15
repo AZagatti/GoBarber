709cde4120576ecd396853d81a5b7e8846c6225e
azagatti <andre.zagatti1@gmail.com>
2020-02-14 23:03:02 -0300

    Created exceptionhandler and configure sentry

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+
+import 'express-async-errors';
+
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
 import './database';
@@ -8,11 +14,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +32,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, res).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
