7849bd111833d00ce1151b9046bf8465e20ff01e
azagatti <andre.zagatti1@gmail.com>
2020-02-14 21:19:44 -0300

    Added nodemailer and create send email

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'f3b4400c92d09a',
+    pass: 'e49b37f8cc3dd2',
+  },
+  default: {
+    from: 'Equipe GoBarbar <noreplay@gobarber.com>',
+  },
+};
